9df1cc9c81d4f0de454836bdad6911f24243c2eb
rbbastos <bastoscanada@gmail.com>
2019-10-07 10:39:21 -0500

    git reset to unistall bootstrap tutorial not work

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,13 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
-// https://gorails.com/forum/install-bootstrap-with-webpack-with-rails-6-beta
-const webpack = require('webpack')
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
-  $: 'jquery',
-  jQuery: 'jquery',
-  Popper: ['popper.js', 'default']
-}))
-
-
-
 module.exports = environment
