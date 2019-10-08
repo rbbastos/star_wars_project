43ea06bf520c96bf9039827754fa65563976e753
rbbastos <bastoscanada@gmail.com>
2019-10-08 17:21:25 -0500

    Routes after all controller + menu link_to all categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,24 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # get 'characters/index'
+  # get 'characters/show'
+  resources :characters, only: %i[index show]
+  # get 'starships/index'
+  # get 'starships/show'
+  resources :starships, only: %i[index show]
+
+  # get 'inhabits/index'
+  resources :inhabits, only: :index
+
+  # get 'species/index'
+  # get 'species/show'
+  resources :species, only: %i[index show]
+
+  # get 'planets/index'
+  # get 'planets/show'
+  resources :planets, only: %i[index show]
+
   resources :pages
 
   get '/static/:permalink', to: 'pages#permalink', as: 'permalink'
