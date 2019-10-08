a616d85638e9b1e75ca278df2d682bcc6da684e2
rbbastos <bastoscanada@gmail.com>
2019-10-08 17:20:13 -0500

    Starship controller and view(incomplete)

---
+++ app/controllers/starships_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class StarshipsController < ApplicationController
+  def index
+    @starships = Starship.order(:name)
+  end
+
+  def show
+    @starship = Starship.find(params[:id])
+  end
+end
